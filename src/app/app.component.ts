71d6e58682fa7131747d396f9cdc033344847461
Omoezone <williamjean.k.o@gmail.com>
2025-04-04 11:54:18 +0200

    added small components for basic page layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { MainComponent } from './layout/main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
